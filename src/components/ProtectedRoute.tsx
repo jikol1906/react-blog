9309c119822074cde914e1728cf4ca736974832e
Boris Grunwald <borisgrunwald@gmail.com>
2025-12-03 19:55:10 +0100

    added protected route

---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,23 @@
+import { Navigate, Outlet, useLocation } from "react-router"; // or "react-router-dom"
+import { useAuth } from "../context/AuthContext";
+
+const ProtectedRoute = () => {
+  const { user, isLoading } = useAuth();
+  const location = useLocation();
+
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <p>Loading user data...</p>
+      </div>
+    );
+  }
+
+  if (!user) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  return <Outlet />;
+};
+
+export default ProtectedRoute;
