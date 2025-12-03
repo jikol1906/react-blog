221a1f45d5b6c745b5d6369ced6dd744eb04861f
Boris Grunwald <borisgrunwald@gmail.com>
2025-12-03 12:45:01 +0100

    append search and filter querystrings

--- src/pages/Article/Articles.tsx
+++ src/pages/Article/Articles.tsx
@@ -1,5 +1,6 @@
 import type { Article } from "@/types";
 import { useFetch } from "@/hooks/useFetch";
+import { useDebounce } from "@/hooks/useDebounce";
 import ArticleCard from "@/components/ArticleCard";
 import {
   InputGroup,
@@ -10,15 +11,30 @@ import { Search } from "lucide-react";
 import { Select, SelectTrigger, SelectValue, SelectContent, SelectGroup, SelectLabel, SelectItem } from "@/components/ui/select";
 
 export default function Articles() {
-  // Der Hook übernimmt alles. Wir geben nur an, welchen Typ <Article[]> wir erwarten.
-  const {
-    data: articles,
-    loading,
-    error,
-  } = useFetch<Article[]>("http://localhost:3000/articles");
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("all");
 
-  if (loading) return <div>Lade Artikel...</div>;
-  if (error) return <div>Fehler: {error}</div>;
+  const debouncedSearch = useDebounce(searchTerm, 200);
+
+  const getQueryURL = () => {
+    const baseUrl = "http://localhost:3000/articles";
+    const params = new URLSearchParams();
+
+    if (debouncedSearch) {
+      params.append("title_like", debouncedSearch);
+    }
+
+    if (selectedCategory && selectedCategory !== "all") {
+      params.append("category", selectedCategory);
+    }
+
+    params.append("_limit", "10");
+
+    const queryString = params.toString();
+    return `${baseUrl}?${queryString}`
+  };
+
+  const { data: articles, loading, error } = useFetch<Article[]>(getQueryURL());
 
   return (
     <div className="py-20">
@@ -29,20 +45,26 @@ export default function Articles() {
       <div className="grid gap-4 max-w-3xl m-auto">
         <div className="grid grid-cols-2 gap-2">
           <InputGroup>
-            <InputGroupInput placeholder="Search..." />
+            <InputGroupInput
+              placeholder="Search..."
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
             <InputGroupAddon>
               <Search />
             </InputGroupAddon>
           </InputGroup>
-          <Select>
+
+          <Select value={selectedCategory} onValueChange={setSelectedCategory}>
             <SelectTrigger className="w-full">
               <SelectValue placeholder="Wähle eine Kategorie" />
             </SelectTrigger>
             <SelectContent>
               <SelectGroup>
                 <SelectLabel>Kategorie</SelectLabel>
-                <SelectItem value="frontend">Frontend</SelectItem>
-                <SelectItem value="backend">Backend</SelectItem>
+                <SelectItem value="all">Alle Kategorien</SelectItem>
+                <SelectItem value="Frontend">Frontend</SelectItem>
+                <SelectItem value="Backend">Backend</SelectItem>
               </SelectGroup>
             </SelectContent>
           </Select>
