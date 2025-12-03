18828066069415f400c0e25769f9702307e859ee
Boris Grunwald <borisgrunwald@gmail.com>
2025-12-03 10:19:31 +0100

    types aktualisieren

--- src/types.ts
+++ src/types.ts
@@ -1,16 +1,26 @@
-export interface Article {
-    id: number;
-    title: string;
-    authorId: number;
-    date: string;
-    category: string;
-    summary: string;
-    content: string;
-    image: string;
-    views: number;
-  }
+export interface User {
+  id: number;
+  username: string;
+  email: string;
+}
 
-  // Neu hinzufügen:
+export interface Article {
+  id: number;
+  title: string;
+  userId: number; 
+  date: string;
+  category: string;
+  summary: string;
+  content: string;
+  image?: string; 
+  views: number;
+}
+
+
+export interface ArticleWithUser extends Article {
+  user: User;
+}
+
 export interface Comment {
   id: number;
   articleId: number;
@@ -19,8 +29,7 @@ export interface Comment {
   date: string;
 }
 
-export interface User {
-    id: number;
-    username: string;
-    email: string;
+
+export interface CommentWithUser extends Comment {
+  user: User;
 }
