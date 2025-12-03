366c50f5c114d1dc2e00b1960675e9d397f0711f
Boris Grunwald <borisgrunwald@gmail.com>
2025-12-03 19:55:50 +0100

    wrapped protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
-import { AuthProvider } from "./context/AuthContext"; // Import the provider
-import { BrowserRouter, Routes, Route } from "react-router";
+import { AuthProvider } from "./context/AuthContext";
+import { BrowserRouter, Routes, Route } from "react-router"; // or "react-router-dom"
 import Login from "./pages/Login/Login";
 import ArticleDetails from "./pages/Article/ArticleDetails";
 import Articles from "./pages/Article/Articles";
 import Layout from "./pages/Layout";
+import ProtectedRoute from "./components/ProtectedRoute"; // Import the guard
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<Articles />} />
-            <Route path="/article/:id" element={<ArticleDetails />} />
-          </Route>
+          
           <Route path="/login" element={<Login />} />
+
+          <Route element={<ProtectedRoute />}>
+            
+            <Route element={<Layout />}>
+              <Route path="/" element={<Articles />} />
+              <Route path="/article/:id" element={<ArticleDetails />} />
+            </Route>
+
+          </Route>
+
         </Routes>
       </BrowserRouter>
     </AuthProvider>
